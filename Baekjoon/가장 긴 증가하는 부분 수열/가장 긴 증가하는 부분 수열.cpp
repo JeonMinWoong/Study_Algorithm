23f813ef58d6790922313658608f1f2a18e33806
JeonMinWoong <alsdnd369@naver.com>
2022-09-07 01:11:07 +0900

    09/06

---
+++ Baekjoon/가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a[1001] = { 0 };
+	int dp[1001];
+	int n,answer = 0;
+    cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+	
+	for (int i = 0; i < n; i++)
+	{
+		dp[i] = 1;
+		for (int j = 0; j < i; j++)
+		{
+			if (a[i] > a[j])
+			{
+				dp[i] = max(dp[j] + 1, dp[i]);
+			}
+		}
+		answer = max(answer, dp[i]);
+	}
+
+	cout << answer << endl;
+}
---
+++ Baekjoon/계단 오르기/계단 오르기.cpp
@@ -0,0 +1,31 @@
+﻿// 계단 오르기.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a[301] = { 0 };
+	int dp[301] = { 0 };
+	int n,answer = 0,cnt = 0;
+
+    cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> a[i];
+	}
+
+	dp[0] = 0;
+	dp[1] = a[1];
+	dp[2] = a[1] + a[2];
+	dp[3] = max(a[1] + a[3], a[2] + a[3]);
+	//dp[4] = max(dp[4-3] + a[4-1] + a[4], dp[4-2] + a[4]);
+	for (int i = 4; i <= n; i++)
+	{
+		dp[i] = max(dp[i - 3] + a[i - 1] + a[i], dp[i - 2] + a[i]);
+	}
+	cout << dp[n] << endl;
+}
